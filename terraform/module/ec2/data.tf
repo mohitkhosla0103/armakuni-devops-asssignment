713390f15d2b23ebd27455ad460da63245d83786
mohit_khosla07 <mohit.khosla07@gmail.com>
2025-08-18 23:16:25 +0530

    new changes in data file

--- terraform/module/ec2/data.tf
+++ terraform/module/ec2/data.tf
@@ -1,11 +1,14 @@
-
-# Amazon Linux AMI
 data "aws_ami" "amazon_linux" {
   most_recent = true
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.6.20250107.0-kernel-6.1-x86_64"]
+    values = ["al2023-ami-*-kernel-6.1-x86_64"]
   }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
